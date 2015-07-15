a640ed2a9521f5ef1a5eaaf02e0f923e79db06a7
tiandarwin <tianjige@163.com>
2015-07-15 11:59:26 +0800

    调整目录结构

--- shardingDataSource/src/main/java/org/darwin/shardingDataSource/dataSource/ReadWriteSplitingDataSource.java
+++ shardingDataSource/src/main/java/org/darwin/shardingDataSource/dataSource/ReadWriteSplitingDataSource.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.darwin.common.utils.ThreadContext;
+import org.darwin.common.ThreadContext;
 import org.darwin.shardingDataSource.dataSource.rule.SlaveSelector;
 import org.darwin.shardingDataSource.dataSource.rule.impl.ByOrderSelector;
 import org.darwin.shardingDataSource.dataSource.rule.impl.ByWeightSelector;
